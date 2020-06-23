d024228ddd0ddbb0aa39efe03fe3d0f64789bdd0
Flavio Francisco <flaviofrancis@msn.com>
2020-06-23 10:30:11 +0200

    Using exposed url: http://a93b2e8de285648bcbc832562fee233f-211218333.eu-central-1.elb.amazonaws.com:8080/api/v0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-reverse-proxy:8080/api/v0'
+  apiHost: 'http://a93b2e8de285648bcbc832562fee233f-211218333.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
