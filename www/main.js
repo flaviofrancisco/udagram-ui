0aad06b45f0f3125b20fd752309a4695116f1f83
Flavio Francisco <flaviofrancis@msn.com>
2020-06-23 10:31:31 +0200

    Ionic build

--- www/main.js
+++ www/main.js
@@ -1955,7 +1955,7 @@ __webpack_require__.r(__webpack_exports__);
 var environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://udagram-reverse-proxy:8080/api/v0'
+    apiHost: 'http://a93b2e8de285648bcbc832562fee233f-211218333.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 /*
  * For easier debugging in development mode, you can import the following file
